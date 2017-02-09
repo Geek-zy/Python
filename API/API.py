d8eab398e8dbf6d4a850d3435ca69a9ba683ef59
Geek-zy <zy251021@163.com>
2017-02-09 17:04:02 +0800

    Python3 调用API接口

---
+++ API/API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+# 安装Python包管理工具pip ==> sudo apt-get install python-pip
+# 安装requests ==> pip install requests
+
+import requests
+
+# 调用API的GET请求
+URL = "http://web.rimag.com.cn/Interfaces/Hospitalreport/fetch?hospitalId=8&pId=P00309195"
+#URL = "http://web.rimag.com.cn/Interfaces/Hospitalreport/fetch"
+Date = requests.get(url = URL)
+#Date = requests.get(url = URL, params={'hospitalId':'8', 'pId':'P00309195'})
+
+# 获取返回状态
+print (Date.status_code)
+# 获取返回URL
+print (Date.url)
+# 获取返回数据
+print (Date.text)
+
