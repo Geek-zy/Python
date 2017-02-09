5e465b8c196187346cb1408bd874a7b3a642c670
Geek-zy <zy251021@163.com>
2017-02-09 17:07:11 +0800

    Python3 操作Json数据

---
+++ Json/Json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import json
+
+Data_Array = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ] 
+Data_Json  = '{"a":1,"b":2,"c":3,"d":4,"e":5}'
+
+# 将 Python 对象编码成 JSON 字符串
+print (json.dumps(Data_Array))
+
+# 将已编码的 JSON 字符串解码为 Python 对象
+print (json.loads(Data_Json))
+
