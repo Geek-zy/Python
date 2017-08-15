a2dbc7d34d00f6bdf13cabcfee64bef033e29d79
Geek-zy <zy251021@163.com>
2017-08-15 10:32:56 +0800

    天干地支纪年法-新版本迭代

---
+++ Calendar/calendar_v0.2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import re
+import os
+
+
+Day = {'1':'甲', '2':'乙', '3':'丙', '4':'丁', '5':'戊', '6':'己', '7':'庚', '8':'辛', '9':'壬', '10':'癸'}
+Ear = {'1':'寅', '2':'卯', '3':'辰', '4':'巳', '5':'午', '6':'未', '7':'申', '8':'酉', '9':'戌', '10':'亥', '11':'子', '12':'丑'}
+Zod = {'1':'鼠', '2':'牛', '3':'虎', '4':'兔', '5':'龙', '6':'蛇', '7':'马', '8':'羊', '9':'猴', '10':'鸡', '11':'狗', '0':'猪'}
+
+def Stock(year):
+    
+    # 如果是公元前需要减一年，因为是负数，所以加一
+    if year < 0:
+        year = year + 1
+
+    # 天干算法 ==> 年份个位 - 3，个位小于等于 3 借 10
+    day = (year % 10) - 3
+    if day <= 0:
+        day = 10 + day
+
+    # 地支算法 ==> 年份 + 7 和 12 取余，余数为 0，则等于 12
+    ear = (year + 7) % 12
+    if ear == 0:
+        ear = 12
+
+    return Day[str(day)] + Ear[str(ear)]
+
+def Zodiac(year):
+    
+    # 年份数 + 9 和12 取余
+    if year > 0:
+        zod = (year + 9) % 12
+    
+    # 公元前的年份要 + 1 之后再 + 9 再和12 取余
+    else:
+        zod = (year + 1 + 9) % 12
+
+    return Zod[str(zod)]
+
+def Main(i):
+    if i == 0:
+        os.system('clear')
+        print('\n * 欢迎使用本【天干地支纪年法】软件!\n')
+
+    year = input("\n * 请输入年份【输入 q 退出程序】: ")
+
+    if year == 'q' or year == 'Q':
+        os.system('clear')
+        print('\n * 感谢使用本【天干地支纪年法】软件，程序已退出!\n')
+        exit(0)
+
+    # 判断用户输入是否是 '-816-201' 这样的字符串
+    data = list(year)
+    data = '-' in data[1:]
+
+    # 判断用户输入是否是 0 - 9 的数字
+    # 判断用户输入是否超过 5 位字符串 包括 '-'
+    # 判断用户输入是否是 空,即直接敲回车
+    if (not re.match(r'^[-?0-9]{1,5}$', year)) or data == True or (year == '-' and len(year) == 1):
+        os.system('clear')
+        print('\n * 年份输入有误,请重新输入! 范围: -9999 ~ 99999 且不等于 0\n')
+        Main(1)
+
+    else:
+        year = int(year)
+        stock  = Stock(year)
+        zodiac = Zodiac(year)
+        
+        if year > 0:
+            os.system('clear')
+            print('\n * 公元 %d 年对应天干地支纪年法是: %s%s 年\n' % (year, stock, zodiac))
+            Main(1)
+
+        elif year < 0:
+            os.system('clear')
+            print('\n * 公元前 %d 年对应天干地支纪年法是: %s%s 年\n' % (abs(year), stock, zodiac))
+            Main(1)
+
+        # 判断用户输入是否是 0
+        else:
+            os.system('clear')
+            print('\n * 年份输入有误,请重新输入! 范围: -9999 ~ 99999 且不等于 0\n')
+            Main(1)
+
+Main(0)
+
