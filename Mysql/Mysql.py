56532d1a20c339280bfd9c1c222177c8931784b1
Geek-zy <zy251021@163.com>
2017-09-06 16:57:55 +0800

    Python3 连接 Mysql 数据库

--- Mysql/Mysql.py
+++ Mysql/Mysql.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 # 安装PyMySQL
@@ -6,28 +6,82 @@
 
 import pymysql
 
-# 打开数据库连接
-db = pymysql.connect("localhost", "root", "root", "Geek_Web" )
 
-# 使用cursor()方法获取操作游标 
-cursor = db.cursor()
+config = {
 
-# 创建数据表SQL语句
-sql = "SELECT * FROM main"
+    'host':'localhost',
+    'port':3306,
+    'user':'root',
+    'passwd':'root',
+    'db':'Geek_Web',
+    'charset':'utf8mb4',
+    # 数据库内容以字典格式输出
+    #'cursorclass':pymysql.cursors.DictCursor,
+}
 
-try:
-    # 执行SQL语句
-    cursor.execute(sql)
-             
-    # 获取所有记录列表
-    results = cursor.fetchall()
+# 连接数据库
+def Mysql():
+
+    # 连接数据库
+    #db = pymysql.connect(host="localhost", port=3306, user="root", passwd="root", db="Geek_Web", charset="utf8mb4")
+    db = pymysql.connect(**config)
+    #cursor()方法获取操作游标 
+    pointer = db.cursor()
+
+    try:
+
+        return (db, pointer)
     
-    # 打印结果
-    print (results)
+    except:
+        
+        print("数据库访问失败")
 
-except:
-    print ("数据访问失败")
+# 增
+def Insert(db, pointer):
+    sql = "insert into main(id, Tag, Name, Version, Introduce, Class, Worked_OS, Course_URL, Download_URL, Image_URL, Remarks_1, Remarks_2) \
+     values (NULL, '软件编号', '软件名称', '软件版本', '软件简介', '软件类别', '运行环境', '教程地址', '下载地址', '图标地址', '备注1', '备注2')"
+    # 执行SQL语句
+    pointer.execute(sql)
+    # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+    db.commit()
+
+# 删
+def Delect(db, pointer):
+    sql = "DELETE FROM main WHERE Name = '修改后的名字'"
+    pointer.execute(sql)
+    db.commit()
+
+# 查
+def Select(db, pointer):
+    sql = "SELECT * FROM main"
+    pointer.execute(sql)
+    # 获取所有记录列表
+    results = pointer.fetchall()
+    
+    return results
+
+# 改
+def Update(db, pointer):
+    sql = "UPDATE main SET Name = '修改后的名字' WHERE Remarks_2 = '备注2'"
+    pointer.execute(sql)
+    db.commit()
 
 # 关闭数据库连接
-db.close()
+def Close(db, pointer):
+    pointer.close()
+    db.close()
+
+(db, pointer) = Mysql()
+print("\n-------------数据库初始状态-------------")
+print(Select(db, pointer))
+Insert(db, pointer)
+print("\n-------------数据库插入数据-------------")
+print(Select(db, pointer))
+Update(db, pointer)
+print("\n-------------数据库修改数据-------------")
+print(Select(db, pointer))
+Delect(db, pointer)
+print("\n-------------数据库删除数据-------------")
+print(Select(db, pointer))
+Close(db, pointer)
 
